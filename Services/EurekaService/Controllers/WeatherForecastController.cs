9f66dc328109a5294617666a0e343b6b99ccdd5b
RicardoAlexandre <ricardo.andrade@napista.com.br>
2021-09-07 20:54:16 -0300

    [Master] Implementação do projeto base

---
+++ Services/EurekaService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Common.Discovery;
+using Steeltoe.Discovery;
+
+namespace EurekaService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EurekaController : ControllerBase
+    {
+
+        private readonly ILogger<EurekaController> _logger;
+        private readonly DiscoveryHttpClientHandler _handler;
+        public EurekaController(ILogger<EurekaController> logger, IDiscoveryClient client)
+        {
+            _logger = logger;
+            _handler = new DiscoveryHttpClientHandler(client);
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var client = new HttpClient(_handler, false);
+            return await client.GetStringAsync("http://ProductService/api/weatherforecast");
+        }
+    }
+}
---
+++ Services/EurekaService/Startup.cs
@@ -0,0 +1,47 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Steeltoe.Common.Http.Discovery;
+using Steeltoe.Discovery.Client;
+
+namespace EurekaService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient("product", c =>
+            {
+                c.BaseAddress = new Uri("http://ProductService/api/weatherforecast/");
+            }).AddServiceDiscovery();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
